f6b710c4fcedd46627010dfff5b8c4e0851e4e44
anthonycasarta <anthonycasarta@gmail.com>
2026-08-13 12:52:03 -0500

    add 00_setup.py and move project setup from 01 to it

---
+++ notebooks/00_setup.py
@@ -0,0 +1,28 @@
+# Create Catalog
+spark.sql(
+        '''
+        CREATE CATALOG IF NOT EXISTS covid19_socioeconomic_analysis;
+        '''
+        )
+
+# COMMAND ----------
+
+# Create schemas
+spark.sql(
+        '''
+        CREATE SCHEMA IF NOT EXISTS covid19_socioeconomic_analysis.bronze;
+        CREATE SCHEMA IF NOT EXISTS covid19_socioeconomic_analysis.silver;
+        CREATE SCHEMA IF NOT EXISTS covid19_socioeconomic_analysis.gold;
+        '''
+        )
+
+# COMMAND ----------
+
+# Create volumes
+spark.sql(
+        '''
+        CREATE VOLUME IF NOT EXISTS covid19_socioeconomic_analysis.bronze.covid_raw;
+        CREATE VOLUME IF NOT EXISTS covid19_socioeconomic_analysis.bronze.gdp_raw;
+        CREATE VOLUME IF NOT EXISTS covid19_socioeconomic_analysis.bronze.healthcare_raw;
+        '''
+        )
