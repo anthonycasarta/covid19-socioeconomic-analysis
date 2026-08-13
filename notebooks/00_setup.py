9e47f5001b59c59ea2c397a1f32fb27709ffe962
anthonycasarta <anthonycasarta@gmail.com>
2026-08-13 13:09:40 -0500

    add make directories for covid, gdp,and healthcare data to 00_setup.py

--- notebooks/00_setup.py
+++ notebooks/00_setup.py
@@ -31,3 +31,18 @@ spark.sql(
         CREATE VOLUME IF NOT EXISTS covid19_socioeconomic_analysis.bronze.healthcare_raw;
         '''
         )
+
+# COMMAND ----------
+
+# Make data directories
+owid_covid_data_raw_dir = "/Volumes/covid19_socioeconomic_analysis/bronze/covid_raw/owid/data/"
+dbutils.fs.mkdirs(owid_covid_data_raw_dir)
+
+world_bank_gdp_data_raw_dir = "/Volumes/covid19_socioeconomic_analysis/bronze/gdp_raw/world_bank/data/"
+world_bank_gdp_metadata_raw_dir = "/Volumes/covid19_socioeconomic_analysis/bronze/gdp_raw/world_bank/metadata/"
+dbutils.fs.mkdirs(world_bank_gdp_data_raw_dir)
+dbutils.fs.mkdirs(world_bank_gdp_metadata_raw_dir)
+
+who_healthcare_data_raw_dir = "/Volumes/covid19_socioeconomic_analysis/bronze/healthcare_raw/who/data/"
+dbutils.fs.mkdirs(who_healthcare_data_raw_dir)
+
