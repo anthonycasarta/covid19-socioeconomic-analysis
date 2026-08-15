a33cc77f2fcc398c8432095c3db3c535c893d205
anthonycasarta <anthonycasarta@gmail.com>
2026-08-14 19:37:52 -0500

    make source directory in healthcare.py be supplied by the bundle instead of hardcoded

--- landing/healthcare.py
+++ landing/healthcare.py
@@ -3,14 +3,14 @@
 # [tool.databricks.environment]
 # environment_version = "5"
 # ///
-user = spark.sql("SELECT current_user()").first()[0]
+dbutils.widgets.text("source_dir", "")
+source_dir = dbutils.widgets.get("source_dir")
 
 # COMMAND ----------
 
 who_healthcare_data_raw_dir = (
     "/Volumes/covid19_socioeconomic_analysis/bronze/healthcare_raw/who/"
 )
-source_dir = f"/Workspace/Users/{user}/covid19-socioeconomic-analysis/data/"
 base_dir = f"{who_healthcare_data_raw_dir}source_a/"
 
 target_dir = base_dir
