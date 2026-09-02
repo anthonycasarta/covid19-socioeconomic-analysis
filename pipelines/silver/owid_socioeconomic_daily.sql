7ad36c1b84957b2e840249d9620534c68fe6d953
anthonycasarta <anthonycasarta@gmail.com>
2026-09-02 11:11:59 -0500

    fix bronze table reference for owid_socioeconomic_daily

--- pipelines/silver/owid_socioeconomic_daily.sql
+++ pipelines/silver/owid_socioeconomic_daily.sql
@@ -7,7 +7,7 @@ select
     gdp_per_capita,
     extreme_poverty
 from stream
-    bronze.owid_covid_raw
+    bronze.covid_owid_raw
 where
     date < '2023-01-01'
     and country not in ('Asia excl. China',  'World', 'World excl. China', 'World excl. China and South Korea', 'World excl. China, South Korea, Japan and Singapore', 'Winter Olympics 2022', 'Summer Olympics 2020', 'Low-income countries', 'Lower-middle-income countries', 'High-income countries', 'European Union (27)')
