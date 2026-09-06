74f38dd7362435be34ca5e92cce3851a22ea9980
anthonycasarta <anthonycasarta@gmail.com>
2026-09-06 14:38:59 -0500

    fix where clause and move covid_gdp_metric_view to pipelines/gold/ directory

--- pipelines/silver/covid_core_metrics_daily.sql
+++ pipelines/silver/covid_core_metrics_daily.sql
@@ -16,7 +16,7 @@ select
 from stream 
     covid_owid_raw
 where
-    and country not in ('Asia excl. China',  'World', 'World excl. China', 'World excl. China, South Korea', 'World excl. China, South Korea, Japan and Singapore', 'Winter Olympics 2022', 'Summer Olympics 2020', 'Low-income countries', 'Lower-middle-income countries', 'High-income countries', 'European Union (27)')
+    country not in ('Asia excl. China',  'World', 'World excl. China', 'World excl. China, South Korea', 'World excl. China, South Korea, Japan and Singapore', 'Winter Olympics 2022', 'Summer Olympics 2020', 'Low-income countries', 'Lower-middle-income countries', 'High-income countries', 'European Union (27)')
     and country is not null
     and date is not null
     and code is not null
--- pipelines/silver/covid_normalized_metrics_daily.sql
+++ pipelines/silver/covid_normalized_metrics_daily.sql
@@ -14,7 +14,7 @@ select
 from stream
     bronze.covid_owid_raw
 where
-    and country not in ('Asia excl. China',  'World', 'World excl. China', 'World excl. China, South Korea', 'World excl. China, South Korea, Japan and Singapore', 'Winter Olympics 2022', 'Summer Olympics 2020', 'Low-income countries', 'Lower-middle-income countries', 'High-income countries', 'European Union (27)')
+    country not in ('Asia excl. China',  'World', 'World excl. China', 'World excl. China, South Korea', 'World excl. China, South Korea, Japan and Singapore', 'Winter Olympics 2022', 'Summer Olympics 2020', 'Low-income countries', 'Lower-middle-income countries', 'High-income countries', 'European Union (27)')
     and country is not null
     and new_cases_per_million is not null
     and new_cases_smoothed is not null
