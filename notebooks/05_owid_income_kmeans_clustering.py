e0af600b9ca5b2c39ddc398d1a3097fac70cd13f
ajcasarta@gmail.com <ajcasarta@gmail.com>
2026-09-06 00:58:57 +0000

    take the gdp value for each country at the earliest year in the owid dataset (2020)

--- notebooks/05_owid_income_kmeans_clustering.py
+++ notebooks/05_owid_income_kmeans_clustering.py
@@ -5,11 +5,26 @@
 # ///
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
+from pyspark.sql import functions as F
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC use catalog covid19_socioeconomic_analysis;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT
+# MAGIC     YEAR(TRY_CAST(date AS DATE)) AS observation_year,
+# MAGIC     COUNT(DISTINCT country) AS countries,
+# MAGIC     COUNT(DISTINCT CASE
+# MAGIC         WHEN TRY_CAST(gdp_per_capita AS DOUBLE) > 0
+# MAGIC         THEN country
+# MAGIC     END) AS countries_with_gdp
+# MAGIC FROM silver.owid_socioeconomic_daily
+# MAGIC GROUP BY observation_year
+# MAGIC ORDER BY observation_year;
 
 # COMMAND ----------
 
@@ -22,14 +37,20 @@ country_gdp = spark.sql(
     from 
         silver.owid_socioeconomic_daily
     where 
-        gdp_per_capita > 0
+        year(date) = 2020
+        and gdp_per_capita > 0
     ;
     '''
 )
 
 # COMMAND ----------
 
-country_gdp.groupBy("country").count().where("count > 1").show()
+display(
+    country_gdp
+    .groupBy("country")
+    .count()
+    .where("count > 1")
+)
 
 # COMMAND ----------
 
@@ -42,9 +63,27 @@ country_gdp_features = assembler.transform(country_gdp)
 
 # COMMAND ----------
 
+display(country_gdp_features)
+
+# COMMAND ----------
+
 model = KMeans(
     k=3,
     seed=42,
     featuresCol="features",
     predictionCol="cluster_id",
 ).fit(country_gdp_features)
+
+# COMMAND ----------
+
+clustered_countries = model.transform(country_gdp_features)
+
+display(
+    clustered_countries
+    .select(
+        "country",
+        "gdp_per_capita",
+        "cluster_id",
+    )
+    .orderBy("gdp_per_capita")
+)
