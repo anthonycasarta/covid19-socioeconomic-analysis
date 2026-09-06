b85f3f06def156e28d95c5dde34ff94a81eebfd3
anthonycasarta <anthonycasarta@gmail.com>
2026-09-06 12:03:35 -0500

    create gold covid_gdp_daily table

---
+++ pipelines/gold/covid_gdp_daily.sql
@@ -0,0 +1,25 @@
+create or refresh streaming table
+    gold.covid_gdp_daily
+as
+select
+    covid.code as country_code,
+    covid.country,
+    covid.continent,
+    covid.date as observation_date,
+    classification.gdp_per_capita,
+    classification.gdp_cluster,
+    covid.population,
+    covid.new_cases,
+    covid.new_deaths,
+    covid.total_cases,
+    covid.total_deaths,
+    covid.stringency_index,
+    covid.reproduction_rate
+    classification.country is not null as is_gdp_classified
+from stream
+    silver.covid_core_metrics_daily as covid
+left join
+    gold.owid_gdp_classification
+on
+    covid.country = classification.country
+;
