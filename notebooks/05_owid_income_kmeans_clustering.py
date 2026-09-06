24706543b1d06ba175c29a93740c1c8dea2bb223
ajcasarta@gmail.com <ajcasarta@gmail.com>
2026-09-06 00:24:26 +0000

    fit model on owid gdp data

--- notebooks/05_owid_income_kmeans_clustering.py
+++ notebooks/05_owid_income_kmeans_clustering.py
@@ -3,4 +3,48 @@
 # [tool.databricks.environment]
 # environment_version = "5"
 # ///
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
 
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use catalog covid19_socioeconomic_analysis;
+
+# COMMAND ----------
+
+country_gdp = spark.sql(
+    '''
+    select distinct
+        country,
+        gdp_per_capita,
+        LN(gdp_per_capita) as log_gdp_per_capita
+    from 
+        silver.owid_socioeconomic_daily
+    where 
+        gdp_per_capita > 0
+    ;
+    '''
+)
+
+# COMMAND ----------
+
+country_gdp.groupBy("country").count().where("count > 1").show()
+
+# COMMAND ----------
+
+assembler = VectorAssembler(
+    inputCols=["log_gdp_per_capita"],
+    outputCol="features",
+)
+
+country_gdp_features = assembler.transform(country_gdp)
+
+# COMMAND ----------
+
+model = KMeans(
+    k=3,
+    seed=42,
+    featuresCol="features",
+    predictionCol="cluster_id",
+).fit(country_gdp_features)
