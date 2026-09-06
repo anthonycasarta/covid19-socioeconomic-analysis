5cc25b9abd3c33bd5cc82082763fda812fa118cc
anthonycasarta <anthonycasarta@gmail.com>
2026-09-06 13:27:24 -0500

    add covid_gdp_metric_view

---
+++ pipelines/gold/covid_gdp_metric_view.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE VIEW covid19_socioeconomic_analysis.gold.covid_gdp_metric_view
+WITH METRICS LANGUAGE YAML AS $$
+version: 1.1
+source: covid19_socioeconomic_analysis.gold.covid_gdp_daily
+
+fields:
+  - name: date
+    expr: observation_date
+  - name: week
+    expr: DATE_TRUNC('WEEK', date)
+  - name: month
+    expr: DATE_TRUNC('MONTH', date)
+  - name: year
+    expr: DATE_TRUNC('YEAR', date)
+  - name: country
+    expr: country
+  - name: continent
+    expr: continent
+  - name: gdp_cluster
+    expr: gdp_cluster
+
+measures:
+  - name: reported_cases
+    expr: SUM(new_cases)
+  - name: reported_deaths
+    expr: SUM(new_deaths)
+  - name: population
+    expr: SUM(population)
+    window:
+      - order: date
+        range: current
+        semiadditive: last
+  - name: cases_per_million
+    expr: MEASURE(reported_cases) * 1000000.0 / NULLIF(MEASURE(population), 0)
+  - name: deaths_per_million
+    expr: MEASURE(reported_deaths) * 1000000.0 / NULLIF(MEASURE(population), 0)
+$$;
