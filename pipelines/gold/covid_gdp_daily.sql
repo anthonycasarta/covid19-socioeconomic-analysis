fc3bd2e0f5b75525dd8478cfc23cfcb92566c81c
anthonycasarta <anthonycasarta@gmail.com>
2026-09-10 14:03:39 -0500

    add total_cases_per_million feature to covid_gdp_daily table

--- pipelines/gold/covid_gdp_daily.sql
+++ pipelines/gold/covid_gdp_daily.sql
@@ -13,9 +13,15 @@ select
     covid.new_deaths,
     covid.total_cases,
     covid.total_deaths,
+    covid_normal.total_cases_per_million,
     classification.country is not null as is_gdp_classified
 from stream
     silver.covid_core_metrics_daily as covid
+left join
+    silver.covid_normalized_metrics_daily as covid_normal
+on
+   covid.country = covid_normal.country
+    and covid.date = covid_normal.date
 left join
     gold.owid_gdp_classification as classification
 on
